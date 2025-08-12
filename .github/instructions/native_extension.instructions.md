7bcc26a9defafc9f1e83ad77dd2784cf651c1a3b
Nic Champagne Williamson <champnic@microsoft.com>
2025-08-12 13:00:17 -0700

    Basic command line functionality

--- .github/instructions/native_extension.instructions.md
+++ .github/instructions/native_extension.instructions.md
@@ -19,19 +19,3 @@ applyTo: "**/*.cpp,**/*.h"
 - When creating a new command that is exported from the extension, ensure that it has help text that is printed out to the command window when the user passes "?" as the first argument to the command.
 - Put the extern methods at the end of the file wrapped in a `extern "C" {}` block.
 
-## Use the following extra files for context
-
-[dbgeng.h](dbgeng.h)
-[utils.h](../../src/utils.h)
-[utils.cpp](../../src/utils.cpp)
-[breakpoints_history.cpp](../../src/breakpoints_history.cpp)
-[breakpoint_list.cpp](../../src/breakpoint_list.cpp)
-[breakpoint_list.h](../../src/breakpoint_list.h)
-[break_commands.cpp](../../src/break_commands.cpp)
-[command_list.cpp](../../src/command_list.cpp)
-[command_list.h](../../src/command_list.h)
-[command_lists.cpp](../../src/command_lists.cpp)
-[command_logger.cpp](../../src/command_logger.cpp)
-[js_command_wrappers.cpp](../../src/js_command_wrappers.cpp)
-[CMakeLists.txt](../../CMakeLists.txt)
-[GenerateStartupCommands.cmake](../../cmake/GenerateStartupCommands.cmake)
--- src/browser_process_manager.cpp
+++ src/browser_process_manager.cpp
@@ -22,18 +22,58 @@ HRESULT CALLBACK DebugExtensionUninitializeInternal() {
 }
 
 HRESULT CALLBACK CommandLineInternal(IDebugClient* client, const char* args) {
-  if (!args || !*args || (args[0] == '?' && args[1] == '\0')) {
+  if (args && *args && (args[0] == '?' && args[1] == '\0')) {
     DOUT(
-        "CommandLine - Manages browser process command line operations.\n\n"
-        "Usage: !CommandLine <operation> [parameters]\n\n"
-        "  <operation>  - Operation to perform (not yet implemented)\n"
-        "  [parameters] - Optional parameters for the operation\n\n"
-        "Note: This command is not yet implemented.\n");
+        "CommandLine - Gets the command line of the current process.\n\n"
+        "Usage: !CommandLine\n\n"
+        "This command displays the command line arguments used to start\n"
+        "the current process being debugged.\n");
     return S_OK;
   }
 
-  // TODO: Implement command line functionality
-  DOUT("CommandLine command is not yet implemented.\n");
+  try {
+    // Get the Process Environment Block (PEB) to access command line
+    std::string peb_command = "!peb";
+    std::string peb_output = utils::ExecuteCommand(&g_debug, peb_command);
+    
+    if (peb_output.empty()) {
+      DOUT("Error: Failed to get PEB information.\n");
+      return E_FAIL;
+    }
+    
+    // Look for CommandLine in the PEB output
+    size_t cmd_line_pos = peb_output.find("CommandLine:");
+    if (cmd_line_pos == std::string::npos) {
+      DOUT("Error: Could not find CommandLine in PEB output.\n");
+      return E_FAIL;
+    }
+    
+    // Extract the command line portion
+    size_t start_pos = peb_output.find("'", cmd_line_pos);
+    if (start_pos == std::string::npos) {
+      DOUT("Error: Could not parse CommandLine from PEB output.\n");
+      return E_FAIL;
+    }
+    start_pos++; // Skip the opening quote
+    
+    size_t end_pos = peb_output.find("'", start_pos);
+    if (end_pos == std::string::npos) {
+      DOUT("Error: Could not find end of CommandLine in PEB output.\n");
+      return E_FAIL;
+    }
+    
+    std::string command_line = peb_output.substr(start_pos, end_pos - start_pos);
+    
+    DOUT("Command Line: %s\n", command_line.c_str());
+    
+  } catch (const std::exception& e) {
+    DOUT("Error retrieving command line: %s\n", e.what());
+    return E_FAIL;
+  } catch (...) {
+    DOUT("Unknown error retrieving command line.\n");
+    return E_FAIL;
+  }
+  
   return S_OK;
 }
 
